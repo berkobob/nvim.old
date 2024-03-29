4bd11d83c6958433b23a3ae3b5dedb30324507c3
Antoine Lever <antoinelever@me.com>
2024-03-29 12:12:43 +0000

    Use <CR> for autocompletion

--- lua/lsp/lsp-zero.lua
+++ lua/lsp/lsp-zero.lua
@@ -14,6 +14,18 @@ return {
 			-- see :help lsp-zero-keybindings
 			-- to learn the available actions
 			lsp_zero.default_keymaps({ buffer = bufnr })
+
+            local cmp = require('cmp')
+
+cmp.setup({
+                preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+  }),
+})
 		end)
 	end,
 }
