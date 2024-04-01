1c0ca29cab28bfad0798cd50b69bb94397fabc97
Antoine Lever <antoinelever@me.com>
2024-04-01 17:03:54 +0100

    Fix eslint issue

--- lua/lsp/none-ls.lua
+++ lua/lsp/none-ls.lua
@@ -1,13 +1,17 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.diagnostics.eslint_d,
-            }
-        })
-    end
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+                null_ls.builtins.diagnostics.write_good,
+                null_ls.builtins.formatting.dart_format
+			},
+		})
+	end,
 }
